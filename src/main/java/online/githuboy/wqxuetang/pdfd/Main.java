3ff5e8fe78a7053d711931679d37c4195a1f0f5a
sweetink <suchu01@hotmail.com>
2020-02-10 21:52:44 +0800

    Update:调整Main启动逻辑

--- src/main/java/online/githuboy/wqxuetang/pdfd/Main.java
+++ src/main/java/online/githuboy/wqxuetang/pdfd/Main.java
@@ -2,6 +2,8 @@ package online.githuboy.wqxuetang.pdfd;
 
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import com.sun.javafx.application.PlatformImpl;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -26,12 +28,23 @@ public class Main extends Application {
     public static void main(String[] args) throws IOException {
         Cli cli = new Cli();
         CommandLine commandLine = cli.get(args);
-        if (null == commandLine) return;
+        if (null == commandLine) System.exit(1);
         HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
         server.createContext("/nvc", new RequestHandler());
         server.start();
         log.info("Start internal http server success,Listen on port:{}", 8081);
+        initFx();
         launch(args);
+    }
+
+    private synchronized static void initFx() {
+        // Note that calling PlatformImpl.startup more than once is OK
+        PlatformImpl.startup(new Runnable() {
+            @Override
+            public void run() {
+                // No need to do anything here
+            }
+        });
     }
 
     @Override
@@ -43,6 +56,9 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
         container.initEngine();
+        stage.setOnCloseRequest(event -> {
+            System.exit(1);
+        });
     }
 
     static class RequestHandler implements HttpHandler {
@@ -50,6 +66,8 @@ public class Main extends Application {
         public void handle(HttpExchange exchange) throws IOException {
             InputStream in = ResourceUtil.getStream("page/fakepage.html");
             byte[] bytes = IoUtil.readBytes(in);
+            Headers headers = exchange.getResponseHeaders();
+            headers.set("Content-Type", "text/html; charset=utf-8");
             exchange.sendResponseHeaders(200, bytes.length);
             OutputStream os = exchange.getResponseBody();
             os.write(bytes);
